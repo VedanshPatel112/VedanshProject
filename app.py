6afa9e27620d062874ad0a408eefa78a0d80b1da
A Chethan Reddy <69640722+chethanreddy123@users.noreply.github.com>
2023-01-05 18:29:04 +0530

    added files

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("[Banpresto](https://www.amazon.com/dp/B093F4P1XQ?ref_=cm_sw_r_cp_ud_dp_K6D1K33AR23HA115FF2C)")
+   st.image("image1.png")
+
+with col2:
+   st.header("A dog")
+   st.image("image2.png")
+
+with col3:
+   st.header("An owl")
+   st.image("image2.png")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("A cat")
+   st.image("image1.png")
+
+with col2:
+   st.header("A dog")
+   st.image("image2.png")
+
+with col3:
+   st.header("An owl")
+   st.image("image2.png")
